9acb4db5ec8beace19c1c2a4a9d90510ed95c12e
Bastar2 <adriiiel.alvarez@gmail.com>
2018-08-29 21:42:01 -0300

    Commit Update Error HTML

--- index.php
+++ index.php
@@ -387,4 +387,4 @@
     <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
     <script type="text/javascript" src="js/template-scripts.js"></script>   
    </body>
-</html>
+</html> 
